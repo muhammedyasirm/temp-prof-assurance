f435d961bda52dd1f94ad58de0a0ba9f319b423d
muhammedyasirm <87080246+muhammedyasirm@users.noreply.github.com>
2024-09-17 09:37:24 +0400

    Add npm build

---
+++ src/components/ContactBanner/conatctBanner.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ContactBanner = () => {
+  return (
+    <>
+      <div className="w-full flex justify-center bg-[#1C1C1C]">
+        <div className="max-w-[30%] text-center py-14">
+            <h2>Contact Us</h2>
+            <div>Logo To be Here</div>
+            <p>
+                Some lorem ispsum content Some lorem ispsum content Some lorem ispsum content Some lorem ispsum content Some lorem ispsum content Some lorem ispsum content Some lorem ispsum content Some lorem ispsum content Some lorem ispsum content Some lorem ispsum content Some lorem ispsum content Some lorem ispsum content Some lorem ispsum content       
+            </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ContactBanner;
--- src/pages/Contact/contactPage.jsx
+++ src/pages/Contact/contactPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'; 
 import ContactForm from '../../components/ContactForm/contactForm';
+import ContactBanner from '../../components/ContactBanner/conatctBanner';
 
 const ContactPage = () => {
     return (
         <>
+            <ContactBanner />
             <ContactForm />
         </>
     )
